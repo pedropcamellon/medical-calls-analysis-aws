f3c1afd8ef57b122261c35454269c5f38cd97431
Pedro P. Camellon <pedropcamellon@gmail.com>
2025-02-03 10:55:57 -0500

    Setup lambdas. Show some demo data.

---
+++ lambda_transcribe.py
@@ -0,0 +1,42 @@
+import boto3
+import json
+import uuid
+
+
+def lambda_handler(event, context):
+    # Extract the bucket name and key from the incoming event
+    bucket = event["Records"][0]["s3"]["bucket"]["name"]
+    key = event["Records"][0]["s3"]["object"]["key"]
+
+    # One of a few different checks to ensure we don't end up in a recursive loop.
+    if not key.endswith(".mp3"):
+        print("This demo only works with mp3 files.")
+        return
+
+    # Create a Boto3 client for the Transcribe service
+    transcribe_client = boto3.client("transcribe", region_name="us-east-1")
+
+    try:
+        # Needs to be a unique name
+        job_name = "transcription-job-" + str(uuid.uuid4())
+
+        transcribe_client.start_transcription_job(
+            TranscriptionJobName=job_name,
+            Media={"MediaFileUri": f"s3://{bucket}/{key}"},
+            MediaFormat="mp3",
+            LanguageCode="en-US",
+            OutputBucketName=bucket,
+            OutputKey=f"{job_name}-transcript.json",
+            Settings={"ShowSpeakerLabels": True, "MaxSpeakerLabels": 2},
+        )
+
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        return {"statusCode": 500, "body": json.dumps(f"Error occurred: {e}")}
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps(
+            f"Submitted transcription job for {key} from bucket {bucket}."
+        ),
+    }
